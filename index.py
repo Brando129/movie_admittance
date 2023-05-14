179ed1150e0d2ea9bbf280c3c68b448c47d5836f
Brandon Colangelo <brandoncancode@gmail.com>
2023-05-14 09:55:13 -0400

    Initial Commit

---
+++ index.py
@@ -0,0 +1,14 @@
+"""Write a function that checks whether a person can
+watch an MA15+ rated movie. One of the following two conditions
+is required for admittance:"""
+
+def can_watch_ma15(age, guardian):
+
+  if age >= 15:
+    return True
+  elif guardian:
+    return True
+  else:
+    return False
+  
+print(can_watch_ma15(13, False))
